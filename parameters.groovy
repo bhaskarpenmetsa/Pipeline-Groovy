669322385023dffa8f0d89ce0f1cca644dfbf23f
bhaskarpenmetsa <45040979+bhaskarpenmetsa@users.noreply.github.com>
2022-01-27 15:57:42 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -3,5 +3,5 @@ env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.TOMCAT_IP=
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
-env.JMETER="https://github.com/harishpallapu/folder.git"
+env.JMETER=
 env.NEXUS_IP=
