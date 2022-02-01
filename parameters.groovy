2437f8a009568f6bae6c2d91e71033febcc2671b
bhaskarpenmetsa <45040979+bhaskarpenmetsa@users.noreply.github.com>
2022-02-01 16:58:42 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,5 +1,5 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.BUILD_GIT_URL=
+env.BUILD_GIT_URL="bhaskar"
 env.Deployment="http://mypublicip:8084"
 env.FUNCTIONAL_WEB_GIT_URL=
 env.FUNCTIONAL_MOBILE_GIT_URL=
